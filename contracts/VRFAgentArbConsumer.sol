2a3b3167d4db536fb1b3876dbe51790dd915ca57
Defi Dev <defi.dev@protonmail.com>
2023-12-28 14:46:51 +0000

    Improve VRF consumer

--- contracts/VRFAgentArbConsumer.sol
+++ contracts/VRFAgentArbConsumer.sol
@@ -9,6 +9,11 @@ interface ArbSys {
     * @return block number as int
      */
     function arbBlockNumber() external view returns (uint);
+    /**
+     * @notice Get Arbitrum block hash (reverts unless currentBlockNum-256 <= arbBlockNum < currentBlockNum)
+     * @return block hash
+     */
+    function arbBlockHash(uint256 arbBlockNum) external view returns (bytes32);
 }
 
 /**
@@ -23,7 +28,8 @@ contract VRFAgentArbConsumer is VRFAgentConsumer {
         uint256 blockNumber = ArbSys(address(100)).arbBlockNumber();
         if (blockNumber == 0) {
             blockNumber = block.number;
+            return uint256(blockhash(blockNumber - 1));
         }
-        return uint256(blockhash(blockNumber - 1));
+        return uint256(ArbSys(address(100)).arbBlockHash(blockNumber - 1));
     }
 }
--- test/randao/VRFTest.t.sol
+++ test/randao/VRFTest.t.sol
@@ -157,15 +157,15 @@ contract VRFTest is AbstractTestHelper {
 
     vm.roll(30);
     vm.warp(fulfillTimestamp + 15);
-    assertEq(agent.jobNextKeeperId(jobKey), 2);
+    assertEq(agent.jobNextKeeperId(jobKey), 1);
     assertEq(consumer.isReadyForRequest(), false);
-    vm.prank(keeperWorker, keeperWorker);
+    vm.prank(alice, alice);
     _callExecuteHelper(
       agent,
       address(job),
       jobId,
       defaultFlags,
-      kid2,
+      kid1,
       new bytes(0)
     );
 
@@ -174,15 +174,15 @@ contract VRFTest is AbstractTestHelper {
 
     vm.roll(40);
     vm.warp(fulfillTimestamp + 31);
-    assertEq(agent.jobNextKeeperId(jobKey), 1);
+    assertEq(agent.jobNextKeeperId(jobKey), 3);
     assertEq(consumer.isReadyForRequest(), true);
-    vm.prank(alice, alice);
+    vm.prank(bob, bob);
     _callExecuteHelper(
       agent,
       address(job),
       jobId,
       defaultFlags,
-      kid1,
+      kid3,
       new bytes(0)
     );
 
